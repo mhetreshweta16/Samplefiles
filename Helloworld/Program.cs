1b41d97fb218c33c619a82cd728b0f905c20c305
mhetreshweta16 <80210970+mhetreshweta16@users.noreply.github.com>
2021-10-05 14:39:41 +0530

    Update Program.cs

--- Helloworld/Program.cs
+++ Helloworld/Program.cs
@@ -14,17 +14,16 @@ namespace Helloworld
         public static void Sub()
         {
             Console.WriteLine("hellow sub");
-<<<<<<< HEAD
+
             Console.WriteLine("modify at local");
-<<<<<<< HEAD
             Console.WriteLine("hiii , hello , this is changes in local");
-=======
+
             Console.WriteLine("hello everyone . this is changes at remote level ");
             
->>>>>>> 020173c46e5f809cded3e2871c23892e47e261b4
-=======
+
+
             Console.WriteLine("modify the code at remote");
->>>>>>> 697550e6133f0c150a12763b861d69cb5a523c2e
+
         }
 
     }
